bc8c30331f80c7264ad1b90dccba786635cb975d
Hongrokgi <Hongrokgi@naver.com>
2022-08-24 08:50:50 +0900

    git생성 후 커밋

---
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -0,0 +1,68 @@
+package study.datajpa.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
+import study.datajpa.dto.MemberDto;
+import study.datajpa.entity.Member;
+
+import javax.persistence.LockModeType;
+import javax.persistence.QueryHint;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
+    List<Member> findTop3HelloBy();
+
+    @Query(name = "Member.findByUsername") // 없어도 찾을 수 있음
+    List<Member> findByUsername(@Param("username") String username);
+
+    @Query("select m from Member m where m.username = :username and m.age = :age")
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
+
+    @Query("select m.username from Member m")
+    List<String> findUsernameList();
+
+    @Query("select new study.datajpa.dto.MemberDto(m.id, m.username, t.name) from Member m join m.team t")
+    List<MemberDto> findMemberDto();
+
+    @Query("select m from Member m where m.username in :names")
+    List<Member> findByNames(@Param("names") Collection<String> names);
+
+    List<Member> findListByUsername(String username);   // 컬렉션
+    Member findMemberByUsername(String username);   // 단건
+    Optional<Member> findOptionalByUsername(String username);   // 단건 Optional 로 감싼 것
+
+    @Query(value = "select m from Member m")
+    Page<Member> findByAge(int age, Pageable pageable); //Pageable import 할 때 주의..
+
+    @Modifying(clearAutomatically = true)
+    @Query("update Member m set m.age = m.age + 1 where m.age >= :age")
+    int bulkAgePlus(@Param("age") int age);
+
+    @Query("select m from Member m left join fetch m.team")
+    List<Member> findMemberFetchJoin();
+
+    @Override
+    @EntityGraph(attributePaths = {"team"})
+    List<Member> findAll();
+
+    @EntityGraph(attributePaths = {"team"})
+    @Query("select m from Member m")
+    List<Member> findMemberEntityGraph();
+
+    //    @EntityGraph("Member.all")
+    @EntityGraph(attributePaths = {"team"}) //fetchJoin 을 사용할 수 있다.
+    List<Member> findEntityGraphByUsername(@Param("username") String username);
+
+    @QueryHints(value = @QueryHint(name = "org.hibernate.readOnly", value = "true"))
+    Member findReadOnlyByUsername(String username);
+
+    //select for update => Lock
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    List<Member> findLockByUsername(String username);
+}
